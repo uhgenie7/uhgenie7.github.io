d979f79f26385caece06acb6c1070fc4ed6687a4
HyejinUm <uhj1993@naver.com>
2022-10-15 14:42:49 +0900

    Fix: 목록에서는 댓글창이 안나오도록 변경

--- src/theme/BlogPostItem.tsx
+++ src/theme/BlogPostItem.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
-import BlogPostItem from '@theme-original/BlogPostItem';
-import Comment from '../components/Comment';
+import React, { useEffect, useRef } from 'react';
+import OriginalBlogPostItem from '@theme-original/BlogPostItem';
+// @ts-expect-error internal code
+import { useBlogPost } from '@docusaurus/theme-common/internal';
+import { useColorMode } from '@docusaurus/theme-common';
 
-export default function BlogPostItemWrapper(props) {
+const utterancesSelector = 'iframe.utterances-frame';
+
+function BlogPostItem(props) {
+  const { colorMode } = useColorMode();
+  const { isBlogPostPage } = useBlogPost();
+  const utterancesTheme = colorMode === 'dark' ? 'github-dark' : 'github-light';
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (!isBlogPostPage) return;
+
+    const utterancesEl = containerRef.current.querySelector(utterancesSelector);
+
+    const createUtterancesEl = () => {
+      const script = document.createElement('script');
+
+      script.src = 'https://utteranc.es/client.js';
+      script.setAttribute('repo', 'uhgenie7/uhgenie7.github.io');
+      script.setAttribute('issue-term', 'pathname');
+      script.setAttribute('label', 'comment');
+      script.setAttribute('theme', utterancesTheme);
+      script.crossOrigin = 'anonymous';
+      script.async = true;
+
+      containerRef.current.appendChild(script);
+    };
+
+    const postThemeMessage = () => {
+      const message = {
+        type: 'set-theme',
+        theme: utterancesTheme,
+      };
+
+      utterancesEl.contentWindow.postMessage(message, 'https://utteranc.es');
+    };
+
+    utterancesEl ? postThemeMessage() : createUtterancesEl();
+  }, [utterancesTheme]);
+
   return (
     <>
-      <BlogPostItem {...props} />
-      <Comment />
+      <OriginalBlogPostItem {...props} />
+      {isBlogPostPage && <div ref={containerRef} />}
     </>
   );
 }
+
+export default BlogPostItem;
