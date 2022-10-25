8851d58c135bd2327fb8ab8b601daa90a5f4711c
uhgenie7 <uhgenie7@gmail.com>
2022-10-25 09:49:17 +0900

    feat: (#5) google seo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   organizationName: 'uhgenie7', // Usually your GitHub org/user name.
   projectName: 'uhgenie7.github.io', // Usually your repo name.
   deploymentBranch: 'gh-pages',
-  trailingSlash: true,
+  trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
