84cce875eaba130907f0316dbb287aaeffd0e369
uhgenie7 <uhgenie7@gmail.com>
2022-11-28 19:12:40 +0900

    chore: sitemap

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,6 +56,8 @@ const config = {
         sitemap: {
           changefreq: 'weekly',
           priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
         },
       }),
     ],
